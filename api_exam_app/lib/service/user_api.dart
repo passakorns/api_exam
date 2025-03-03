3c2aeafdf277a7a24bfa6e39dc9db1ae3e505391
passakorns <52370159+passakorns@users.noreply.github.com>
2025-03-03 08:00:56 +0700

    Update user_api.dart

--- api_exam_app/lib/service/user_api.dart
+++ api_exam_app/lib/service/user_api.dart
@@ -5,7 +5,7 @@ import 'package:api_exam_app/model/user_name.dart';
 class UserApi {
   static Future<List<User>> fetchUsers() async {
     
-    const url = 'https://randomuser.me/api/?results=50';
+    const url = '';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
     final body = response.body;
@@ -13,16 +13,13 @@ class UserApi {
     final results = json['results'] as List<dynamic>;
     final users = results.map((person){
       final name = UserName(
-        title: person['name']['title'],
         first: person['name']['first'],
         last: person['name']['last'],
       );
       return User(
         cell: person['cell'],
-        email: person['email'],
         gender: person['gender'],
         picture: person['picture']['thumbnail'],
-        phone: person['phone'],
         name: name
       );
     }).toList();
